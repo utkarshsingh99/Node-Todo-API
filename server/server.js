e97cb7e18861cc033085e44ff1b4f4728e041765
utkarshsingh99 <utkarshsingh369@gmail.com>
2018-06-24 16:43:50 +0530

    Added get resource

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {        //resource creation
     });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
