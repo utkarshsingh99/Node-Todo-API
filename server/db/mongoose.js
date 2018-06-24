0d0bcc5a59f1a82b2c3143306d3e955120bf9340
utkarshsingh99 <utkarshsingh369@gmail.com>
2018-06-24 16:49:22 +0530

    Rectifying address

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://utkarshsingh99:Lordbabaji@123@ds261540.mlab.com:61540/node-todo-utki' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://utkarshsingh99:utki123@ds261540.mlab.com:61540/node-todo-utki' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose}
