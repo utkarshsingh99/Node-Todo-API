750b70ed3d8e0c3b135e523814e25bb2e4483b3c
utkarshsingh99 <utkarshsingh369@gmail.com>
2018-06-25 13:04:41 +0530

    Added link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://utkarshsingh99:utki123@ds261540.mlab.com:61540/node-todo-utki' || 'mongodb://localhost:27017/TodoApp');
 
 // 'mongodb://utkarshsingh99:utki123@ds261540.mlab.com:61540/node-todo-utki' ||
 module.exports = {mongoose}
